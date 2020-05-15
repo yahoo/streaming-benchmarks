e1e57e634ffae18f1954626c0fd1836740807ea2
schintap <schintap@verizonmedia.com>
2020-05-15 15:09:08 -0400

    checkpoint apache beam

--- apache-beam-validator/src/main/java/apache/beam/AdvertisingBeamStream.java
+++ apache-beam-validator/src/main/java/apache/beam/AdvertisingBeamStream.java
@@ -4,11 +4,18 @@
  */
 package apache.beam;
 
-import org.json.JSONObject;
+import avro.shaded.com.google.common.collect.ImmutableMap;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.*;
+import org.apache.beam.sdk.values.KV;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.beam.sdk.io.kafka.KafkaIO;
 
-import java.util.*;
 
 /**
  * To Run:
@@ -19,6 +26,43 @@ public class AdvertisingBeamStream {
 
     public static void main(final String[] args) throws Exception {
 
+        PipelineOptions options = PipelineOptionsFactory.create();
+
+        // Create the Pipeline object with the options we defined above.
+        Pipeline p = Pipeline.create(options);
+
+        p.apply(KafkaIO.<String, String>read()
+                .withBootstrapServers("localhost:9092")
+                .withTopic("ad-events")
+                .withKeyDeserializer(StringDeserializer.class)
+                .withValueDeserializer(StringDeserializer.class)
+
+                .updateConsumerProperties(ImmutableMap.of("auto.offset.reset", (Object)"earliest"))
+
+                // We're writing to a file, which does not support unbounded data sources. This line makes it bounded to
+                // the first 5 records.
+                // In reality, we would likely be writing to a data source that supports unbounded data, such as BigQuery.
+                .withMaxNumRecords(5)
+
+                .withoutMetadata() // PCollection<KV<Long, String>>
+        )
+                .apply(Values.<String>create())
+                .apply("ExtractWords", ParDo.of(new DoFn<String, String>() {
+                    @ProcessElement
+                    public void processElement(ProcessContext c) {
+
+                    }
+                }))
+                .apply(Count.<String>perElement())
+                .apply("FormatResults", MapElements.via(new SimpleFunction<KV<String, Long>, String>() {
+                    @Override
+                    public String apply(KV<String, Long> input) {
+                        return input.getKey() + ": " + input.getValue();
+                    }
+                }))
+                .apply(TextIO.write().to("wordcounts"));
+
+        p.run().waitUntilFinish();
 
     }
 }
