c964227d4595308333b127a07abf603afef8dbfd
schintap <schintap@verizonmedia.com>
2020-06-15 15:52:15 -0400

    remove apex.xml and add kafka env

--- scripts/kafka_env.sh
+++ scripts/kafka_env.sh
@@ -6,4 +6,4 @@ SCALA_VERSION=${SCALA_VERSION:-"2.12"}
 
 sed -i -e 's/broker.id=0/broker.id=$i/' ~/streaming-benchmarks/kafka_$SCALA_VERSION-$KAFKA_VERSION/config/server.properties
 
-sed -i -e 's/zookeeper.connect=localhost:2181/zookeeper.connect=10.227.218.254:2181,10.227.218.253:2181,10.227.218.253:2181/' ~/streaming-benchmarks/kafka_$SCALA_VERSION-$KAFKA_VERSION/config/server.properties
+sed -i -e 's/zookeeper.connect=localhost:2181/zookeeper.connect=10.227.218.254:2181,10.227.218.253:2181,10.227.218.252:2181/' ~/streaming-benchmarks/kafka_$SCALA_VERSION-$KAFKA_VERSION/config/server.properties
